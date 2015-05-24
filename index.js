62aebb4a9ecc49919dcd0131ecfe3e6acd45b305
Jason Baker <amnorvend@gmail.com>
2015-05-24 10:09:43 -0700

    Add the ability to output individual folios into their own files.

--- index.js
+++ index.js
@@ -11,22 +11,31 @@ exports.extractText = function(varsObj) {
   }
   // Yes I'm synchronously slurping a text file. Sue me.
   var splitLines = fs.readFileSync(varsObj.fname, {encoding: 'utf8'}).split('\n');
-  var selectedLines = splitLines.filter(function(val) { 
+  var selectedLines = splitLines.filter(function(val) {
     return val.search(regex) !== -1;
   });
   var strippedTextLines = selectedLines.map(function(val) {
-    return val.match(regex)[5];
+    var match = val.match(regex);
+    var line = processLine(match[5]);
+    if (varsObj.filePerFolio) {
+      var fname = match[1];
+      fs.appendFileSync(varsObj.outFile + '/' + fname + '.txt', line);
+    }
+    return line;
   });
 
-  // VOMIT: This is horrendously repetitive, and it seems like some kind of
-  // pipeline/monad waiting to happen.
-  var fullText = strippedTextLines.join('\n');
-  fullText = fullText.split('.').join(' ').replace(/\!/g, '');
-  fullText = fullText.replace(/=/g, '\n');
-  fullText = fullText.replace(/\{.*\}/g, '');
-  fullText = fullText.replace(/-$/, '');
-  return fullText.replace(/-/g, ' ');
+  return strippedTextLines.join('\n');
 };
+
+function processLine(line) {
+  return line
+    .split('.').join(' ')
+    .replace(/=/, '\n')
+    .replace(/\{.*\}/g, '')
+    .replace(/-$/, '')
+    .replace(/-/, ' ')
+    .replace(/!|%/g, '');
+}
 
 var varsObjDefaults = {
   // Matches any folio
@@ -40,6 +49,13 @@ var varsObjDefaults = {
 
   // Takeshi Takahashi
   transcriber: 'H',
+
+  // File or directory to write output to
+  outFile: null,
+
+  // If given, outFile must be a directory. This outputs one file for each
+  // folio.
+  filePerFolio: false,
 
   // Transcript included in bundle
   fname: __dirname + '/voynich.txt'
